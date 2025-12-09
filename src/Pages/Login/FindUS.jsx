7570828091e28586a409eafd6d3adc330066d939
Bijoy-karmokar <bijoykarmokar320@gmail.com>
2025-12-09 18:53:53 +0600

    Find us

--- src/Pages/Login/FindUS.jsx
+++ src/Pages/Login/FindUS.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const FindUS = () => {
   return (
     <div>
-      <h3>Find Us On</h3>
-      <div className="join join-vertical">
-        <button className="btn join-item">Button</button>
-        <button className="btn join-item">Button</button>
-        <button className="btn join-item">Button</button>
+      <h3 className="text-xl font-medium mt-5">Find Us On</h3>
+      <div className="join join-vertical w-full">
+        <button className="btn join-item"><FaFacebook></FaFacebook> Facebook</button>
+        <button className="btn join-item"><FaTwitter></FaTwitter> Twitter</button>
+        <button className="btn join-item"><FaInstagram></FaInstagram> Instagram</button>
       </div>
     </div>
   );
--- src/components/AsideSection/RightAside.jsx
+++ src/components/AsideSection/RightAside.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SocialLogIn from '../../Pages/Login/SocialLogIn';
+import FindUS from '../../Pages/Login/FindUS';
 
 const RightAside = () => {
     return (
         <div>
            <SocialLogIn></SocialLogIn>
+           <FindUS></FindUS>
         </div>
     );
 };
