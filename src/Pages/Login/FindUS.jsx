7d94b5990d04bfb9e02fc4d3413bd4e7ce21b70e
Bijoy-karmokar <bijoykarmokar320@gmail.com>
2025-12-09 18:49:12 +0600

    sociallogin

---
+++ src/Pages/Login/FindUS.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FindUS = () => {
+  return (
+    <div>
+      <h3>Find Us On</h3>
+      <div className="join join-vertical">
+        <button className="btn join-item">Button</button>
+        <button className="btn join-item">Button</button>
+        <button className="btn join-item">Button</button>
+      </div>
+    </div>
+  );
+};
+
+export default FindUS;
--- src/Pages/Login/SocialLogIn.jsx
+++ src/Pages/Login/SocialLogIn.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
 
 const SocialLogIn = () => {
     return (
         <div>
-            <h3 className='text-lg font-semibold'>LogIn with</h3>
-            <button className='btn'>Login With Google</button>
-            <button className='btn'>Login With Github</button>
+            <h3 className='text-lg font-semibold mb-3'>LogIn with</h3>
+           <div className='flex flex-col gap-3'>
+             <button className='btn btn-outline btn-info'><FcGoogle size={20}></FcGoogle> Login With Google</button>
+            <button className='btn btn-outline'><FaGithub size={20}></FaGithub> Login With Github</button>
+           </div>
         </div>
     );
 };
